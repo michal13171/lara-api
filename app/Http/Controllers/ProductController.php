67bfe0f55fd33b08911d2760ec339986c62c0a60
Michał Kukuła <michal131711@gmail.com>
2019-01-22 13:25:02 +0100

    app source data

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Product[]|\Illuminate\Database\Eloquent\Collection
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $product = new Product([
+            'name' => $request->name,
+            'amount'=> $request->amount,
+        ]);
+         $product->save();
+        return http_response_code(200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+        return $product;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $product = Product::findOrFail($id);
+        return $product;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $product = Product::findOrFail($id);
+        $product->update($request->all());
+        return http_response_code(201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $product = Product::findOrFail($id);
+        $product->delete();
+
+        return http_send_status(204);
+    }
+}
---
+++ database/seeds/ProductTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Model\Product;
+use Illuminate\Database\Seeder;
+
+class ProductTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Let's truncate our existing records to start from scratch.
+        Product::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        // And now, let's create a few articles in our database:
+        for ($i = 0; $i < 50; $i++) {
+            Product::create([
+                'name' => $faker->name,
+                'amount' => $faker->numberBetween(3,15),
+            ]);
+        }
+    }
+}
